129f5cffd5bdc41d2aa22d1c7f5204f83be90c44
Antony <akb@akb.me.uk>
2014-04-21 19:17:50 +0100

    Parser integrated, and syntax highlighted.

--- main.js
+++ main.js
@@ -17,6 +17,96 @@ $(document).ready(function()
     $("#taGrammar").next().addClass("SourceEditor");
     $("#taParseMe").next().addClass("SourceEditor");
     $("#taOutput").next().addClass("OutputWindow");
+    
+    function hightlightSyntax(lexer, types, cm)
+    {
+        lexer.setInput(cm.getValue());
+//        jQuery('#tokens').html('');
+
+        while (!lexer.done)
+        {
+            var typeId = lexer.lex();
+
+            var colorClass = "Black";
+
+            switch(typeId % 6)
+            {
+            case 0:
+                colorClass = "Red";
+                break;
+            case 1:
+                colorClass = "Orange";
+                break;
+            // Yellow is too hard to see on a white background
+            case 2:
+                colorClass = "Green";
+                break;
+            case 3:
+                colorClass = "Blue";
+                break;
+            case 4:
+                colorClass = "Indigo";
+                break;
+            case 5:
+                colorClass = "Violet";
+                break;
+            }
+
+            cm.doc.markText({line: lexer.yylloc.first_line - 1, ch: lexer.yylloc.first_column},
+                            {line: lexer.yylloc.last_line - 1,  ch: lexer.yylloc.last_column},
+                            {className: colorClass});
+        }
+    };
+    
+    /* What follows is based on http://zaach.github.io/jison/try/ */
+
+    $("#process_btn").click(processGrammar);
+//    $("#parse_btn").click(runParser); 
+
+    function processGrammar()
+    {
+        var type = "lalr";
+
+        var grammar = cmGrammar.getValue();
+
+        try
+        {
+            var cfg = bnf.parse(grammar);
+        }
+        catch (e)
+        {
+            cmOutput.setValue("Oops. Make sure your grammar is in the correct format.");
+            $("#taOutput").next().addClass('bad');
+            return;
+        }
+
+        Jison.print = function () {};
+        parser = Jison.Generator(cfg, {type: type});
+
+        $("#out").removeClass("good").removeClass("bad").html('');
+        $("#gen_out").removeClass("good").removeClass("bad");
+        if (!parser.conflicts)
+        {
+            cmOutput.setValue('Generated successfully!');
+            $("#taOutput").next().addClass('good');
+        }
+        else
+        {
+            cmOutput.setValue('Conflicts encountered:<br/>');
+            $("#taOutput").next().addClass('bad');
+        }
+
+        parser.resolutions.forEach(function (res)
+        {
+            var r = res[2];
+            if (!r.bydefault) return;
+            $("#gen_out").append(r.msg+"\n"+"("+r.s+", "+r.r+") -> "+r.action);
+        });
+
+        parser2 = parser.createParser();
+
+        hightlightSyntax(parser.lexer, parser.terminals_, cmParseMe);
+    }
 
 });
 
